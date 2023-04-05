156221690694b7233fbb002099c7beefd0bbb496
unknown <dksgpfls06@naver.com>
2023-04-05 16:23:18 +0900

    code merge success

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, {useEffect, useState} from "react";
+import Makemenu from "./component/nav";
 import { firestore } from "./firebase";
 
 const { kakao } = window;
@@ -29,12 +30,17 @@ useEffect(() => {
   const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
   const options = { 
       center: new kakao.maps.LatLng(33.450701, 126.570667), 
-      level: 3 
+      level: 3
+      // 클러스터러 사용 시
+      // level: 10
   };
 
   const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
   const db = firestore.collection("map"); // firestore collection 접근
+
+      //처음에 옆에 창 안열리게 설정
+      document.getElementById("first").style.display="none";
 
   // firestore에 있는 data 불러오는 부분 -> 해당 data 이용하는 함수 생성 시 이 안에서 짜야함
   db.get().then((result) => { result.forEach( (allDoc) => {
@@ -60,11 +66,11 @@ useEffect(() => {
   });
 
 
-  var cctvimg = '/image/cctv.png';
+  var cctvimg = '/img/cctv.png';
 
   displayCCTVMarker(cctvPosition, cctvimg);
 
-  var mylocationimg = '/image/marker.png';
+  var mylocationimg = '/img/marker.png';
   
   if (navigator.geolocation) {
       
@@ -107,6 +113,13 @@ useEffect(() => {
     map.setCenter(locPosition);     
     
   }
+
+    //사진 담을 div를 아이디로 불러옴
+    var viewimg = document.getElementById("viewimg");
+    //장소 이름 적을 span을 아이디로 불러옴
+    var place_name = document.getElementById("place_name");
+    place_name.appendChild(document.createTextNode(""));
+
     // CCTV 마커 표시 함수
     function displayCCTVMarker(cctvPosition, imageaddress) {
   
@@ -118,11 +131,46 @@ useEffect(() => {
       var cctvmarker = new kakao.maps.Marker({  
           map: map, 
           position: cctvPosition,
+          title: allDoc.data().title,
           image: markerImage,
           zIndex: 2,
       }); 
 
-      map.setCenter(cctvPosition); 
+    map.setCenter(cctvPosition); 
+
+      // 클러스터러 그릴 마커 넣기
+     var markers = [];
+     markers.push(cctvmarker);
+
+  //   // 마커 클러스터러 생성
+  //   var clusterer = new kakao.maps.MarkerClusterer({
+  //   map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+  //   averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+  //   minLevel: 5 // 클러스터 할 최소 지도 레벨 
+  // });
+
+  // clusterer.addMarkers(markers);
+      
+
+      // 옆에 창 여는 클로저를 만드는 함수
+    function openwd(src, name) {
+    return function(){
+    document.getElementById("first").style.display="block";
+    viewimg.style.backgroundImage="url("+src+")";
+    document.getElementById("place_name").innerHTML = name;
+    document.getElementById("people_cnt").innerHTML = 'test하는 중..';
+    }
+
+
+}
+
+var close_btn = document.getElementById("close_btn");
+close_btn.addEventListener('click',function(){
+document.getElementById("first").style.display="none";
+
+})
+
+kakao.maps.event.addListener(cctvmarker, 'click', openwd(allDoc.data().imgsrc, allDoc.data().title));  
 
  // cctvmarker 마우스오버 시 circle animation 그려짐
 kakao.maps.event.addListener(cctvmarker, 'mouseover', function() {
@@ -154,15 +202,16 @@ kakao.maps.event.addListener(cctvmarker, 'mouseout', function() {
 
   return(
   
+    <div>
   <div id='map'
   style={{
     width:'100%', 
     height:'100vh',
     position: 'relative',
   }}
-  >
+  ></div>
+  <Makemenu></Makemenu>
   </div>
-
   )
 }
 
